4eae6c7becc2a7405b260af8b1c20ac68983b54b
Lucas Phelipe Castanho Ribeiro <pheliperibeiro2003@gmail.com>
2023-06-14 02:10:10 -0300

    feat: add initial html structure and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.scss'
-import { Inter } from 'next/font/google'
+import "./globals.scss";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600"],
+});
 
 export const metadata = {
-  title: 'Lucas Phelipe Castanho Ribeiro',
-  description: 'Lucas Ribeiro is a software engineer...',
-}
+    title: "Lucas Phelipe Castanho Ribeiro",
+    description: "Lucas Ribeiro is a software engineer...",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
